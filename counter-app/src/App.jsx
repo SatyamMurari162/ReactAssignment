0ab2a7b38beead3b403c99c4cfd73e4422b5e193
SATYAM MURARI <137689968+SatyamMurari162@users.noreply.github.com>
2024-01-28 13:33:30 +0530

    Update App.jsx

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> 641859fdd6df20439bdc3580ded52cc71f5a4488
 import './App.css'
 import Counter from './components/Counter'
 function App() {
   
-<<<<<<< HEAD
 
-=======
->>>>>>> 641859fdd6df20439bdc3580ded52cc71f5a4488
   return (
     <>
       <Counter></Counter>
